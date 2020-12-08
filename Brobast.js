cbac3d264f934cce01c454cd42824d74b40eb498
aymenbne548485 <57303307+aymenbne548485@users.noreply.github.com>
2020-12-08 03:04:39 +0100

    Add files via upload

---
+++ Brobast.js
@@ -0,0 +1,125 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://----.glitch.me`);
+}, 280000);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "&";
+const Canvas = require("canvas");
+const fs = require('fs');
+const moment = require('moment');
+const sql = require('sqlite');
+const ms = require('ms');
+const jimp = require('jimp');
+
+// ========================================== [ CONSTRUCTERS ] =========================================
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`REEEE™`,"https://discord.gg/JeWeAs")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "bcall")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : Messages received`); 
+message.delete(); 
+};     
+});
+
+
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "bcoff")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Messages received`); 
+message.delete(); 
+};     
+});
+
+
+client.on("message", message => {
+
+  if (message.content.startsWith(prefix + "bcon")) {
+               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'offline').size}\` : عدد الاعضاء المستلمين`); 
+message.delete(); 
+};     
+});
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'brembed') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle('Invite link')
+            .setURL('https://discord.gg/FkRRjKM')
+            .setAuthor('Contrition E-Sports ™', 'https://i.imgur.com/cOpr1tH.jpg')
+            .addField('Message :', args)
+            .setThumbnail('https://i.imgur.com/ghkBzuT.gif')
+            .setTimestamp()
+            .setFooter('See you there', 'https://i.imgur.com/ghkBzuT.gif');
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+client.login(process.env.TOKEN);
